b220ec794de388dad33774208374629610c9f4ab
Johnny Briones <jbrrii91@gmail.com>
2020-12-06 18:07:59 -0500

    StarWarsPagination

--- src/components/StarWarsPagination.js
+++ src/components/StarWarsPagination.js
@@ -9,19 +9,19 @@ class StarWarsPagination extends React.Component {
       
         <nav aria-label="Page navigation example">
           <br />
-        <ul class="pagination justify-content-center">
-          <li class="page-item"><a class="page-link" href="#">Previous</a></li>
-          <li class="page-item"><a class="page-link" href="#">1</a></li>
-          <li class="page-item"><a class="page-link" href="#">2</a></li>
-          <li class="page-item"><a class="page-link" href="#">3</a></li>
-          <li class="page-item"><a class="page-link" href="#">4</a></li>
-          <li class="page-item"><a class="page-link" href="#">5</a></li>
-          <li class="page-item"><a class="page-link" href="#">6</a></li>
-          <li class="page-item"><a class="page-link" href="#">7</a></li>
-          <li class="page-item"><a class="page-link" href="#">8</a></li>
-          <li class="page-item"><a class="page-link" href="#">9</a></li>
-          <li class="page-item"><a class="page-link" href="#">10</a></li>
-          <li class="page-item"><a class="page-link" href="#">Next</a></li>
+        <ul className="pagination justify-content-center">
+          <li className="page-item"><a className="page-link" href="#">Previous</a></li>
+          <li className="page-item"><a className="page-link" href="#">1</a></li>
+          <li className="page-item"><a className="page-link" href="#">2</a></li>
+          <li className="page-item"><a className="page-link" href="#">3</a></li>
+          <li className="page-item"><a className="page-link" href="#">4</a></li>
+          <li className="page-item"><a className="page-link" href="#">5</a></li>
+          <li className="page-item"><a className="page-link" href="#">6</a></li>
+          <li className="page-item"><a className="page-link" href="#">7</a></li>
+          <li className="page-item"><a className="page-link" href="#">8</a></li>
+          <li className="page-item"><a className="page-link" href="#">9</a></li>
+          <li className="page-item"><a className="page-link" href="#">10</a></li>
+          <li className="page-item"><a className="page-link" href="#">Next</a></li>
         </ul>
       </nav>
     );
