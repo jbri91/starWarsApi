b7eead1a384f380bfe8ae161cf54f8307bfe8eff
Johnny Briones <jbrrii91@gmail.com>
2020-12-09 22:45:51 -0500

    format

--- src/App.js
+++ src/App.js
@@ -1,90 +1,102 @@
-import React from 'react'
-import Table from './components/Table'
-import UserInput from './components/UserInput'
-import './App.css';
-import StarWarsPagination from './components/StarWarsPagination'
-
-
+import React from "react";
+import Table from "./components/Table";
+import UserInput from "./components/UserInput";
+import "./App.css";
+import StarWarsPagination from "./components/StarWarsPagination";
 
 // import axios from 'axios'
 // let date = new Date
 
 class App extends React.Component {
   constructor(props) {
-    super(props)
-    
+    super(props);
+
     this.state = {
-     activePage: 1,
-     loading: false,
-     character: []
+      activePage: 1,
+      loading: false,
+      character: [],
+    };
+    this.handlePageChange = this.handlePageChange.bind(this);
+    this.handleDataRequest = this.handleDataRequest.bind(this);
   }
- this.handlePageChange = this.handlePageChange.bind(this)
-  this.handleDataRequest = this.handleDataRequest.bind(this)
-}
 
-handlePageChange(pageNumber) {
-  this.setState({
-    activePage: pageNumber
-  })
-}
+  handlePageChange(pageNumber) {
+    this.setState({
+      activePage: pageNumber,
+    });
+  }
 
-componentDidUpdate(prevProps, prevState) {
- if(this.state.activePage !== prevState.activePage) {
-  this.handleDataRequest(); 
- }
-}
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.activePage !== prevState.activePage) {
+      this.handleDataRequest();
+    }
+  }
 
-componentDidMount() {
-this.handleDataRequest();  
-}
+  componentDidMount() {
+    this.handleDataRequest();
+  }
 
-handleDataRequest() {
-  this.setState({
-    loading: true
-  })
-  
-  Promise.all([
-  fetch(`https://swapi.dev/api/people/?page=${this.state.activePage}`).then(people => people.json()),
-  fetch(`https://swapi.dev/api/planets/?page=${this.state.activePage}`).then(planets => planets.json()),
-  fetch(`https://swapi.dev/api/species/?page=${this.state.activePage}`).then(species => species.json())
-])
-.then(response => this.setState({ 
-  loading: false, 
-  character: [...response[0].results], 
-  planets: [...response[1].results],
-  species: [...response[2].results]
-}))
-  .catch(error => {console.log(error);});
+  handleDataRequest() {
+    this.setState({
+      loading: true,
+    });
 
-}
+    Promise.all([
+      fetch(
+        `https://swapi.dev/api/people/?page=${this.state.activePage}`
+      ).then((people) => people.json()),
+      fetch(
+        `https://swapi.dev/api/planets/?page=${this.state.activePage}`
+      ).then((planets) => planets.json()),
+      fetch(
+        `https://swapi.dev/api/species/?page=${this.state.activePage}`
+      ).then((species) => species.json()),
+    ])
+      .then((response) =>
+        this.setState({
+          loading: false,
+          character: [...response[0].results],
+          planets: [...response[1].results],
+          species: [...response[2].results],
+        })
+      )
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 
- 
-
-
- render() {
-console.log(this.state.planets)
-  return (
-    <div className="App">   
-          <h1 style=
-          {{ 
-            fontSize: '60px', 
-            color: 'yellow'
-          }}>Star Wars API</h1>
-      <UserInput />
-      {this.state.loading ? <h1 style={{color: 'yellow'}}>Loading...</h1> : 
-      <Table 
-        character={this.state.character}
-        planets={this.state.planets}
-        species={this.state.species}
-      />}
-      <StarWarsPagination
-      activePage={this.state.activePage}
-      itemsCountPerPage={10}
-      totalItemsCount={82}
-      pageRangeDisplayed={10}
-      handlePageChange={this.handlePageChange} />
-    </div>
-  );}
+  render() {
+    console.log(this.state.planets);
+    return (
+      <div className="App">
+        <h1
+          style={{
+            fontSize: "60px",
+            color: "yellow",
+          }}
+        >
+          Star Wars API
+        </h1>
+        <UserInput />
+        {this.state.loading ? (
+          <h1 style={{ color: "yellow" }}>Loading...</h1>
+        ) : (
+          <Table
+            character={this.state.character}
+            planets={this.state.planets}
+            species={this.state.species}
+          />
+        )}
+        <StarWarsPagination
+          activePage={this.state.activePage}
+          itemsCountPerPage={10}
+          totalItemsCount={82}
+          pageRangeDisplayed={10}
+          handlePageChange={this.handlePageChange}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/StarWarsPagination.js
+++ src/components/StarWarsPagination.js
@@ -1,24 +1,59 @@
 import React from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function StarWarsPagination(props) {
-    return (
-        <nav  aria-label="Page navigation example">
-          <br />
-        <ul className="pagination justify-content-center">
-          <li onClick={() => props.handlePageChange(1)} className="page-item"><a className="page-link" href="#">1</a></li>
-          <li onClick={() => props.handlePageChange(2)} className="page-item"><a className="page-link" href="#">2</a></li>
-          <li onClick={() => props.handlePageChange(3)} className="page-item"><a className="page-link" href="#">3</a></li>
-          <li onClick={() => props.handlePageChange(4)} className="page-item"><a className="page-link" href="#">4</a></li>
-          <li onClick={() => props.handlePageChange(5)} className="page-item"><a className="page-link" href="#">5</a></li>
-          <li onClick={() => props.handlePageChange(6)} className="page-item"><a className="page-link" href="#">6</a></li>
-          <li onClick={() => props.handlePageChange(7)} className="page-item"><a className="page-link" href="#">7</a></li>
-          <li onClick={() => props.handlePageChange(8)} className="page-item"><a className="page-link" href="#">8</a></li>
-          <li onClick={() => props.handlePageChange(9)} className="page-item"><a className="page-link" href="#">9</a></li>
-        </ul>
-      </nav>
-    );
+  return (
+    <nav aria-label="Page navigation example">
+      <br />
+      <ul className="pagination justify-content-center">
+        <li onClick={() => props.handlePageChange(1)} className="page-item">
+          <a className="page-link" href="#">
+            1
+          </a>
+        </li>
+        <li onClick={() => props.handlePageChange(2)} className="page-item">
+          <a className="page-link" href="#">
+            2
+          </a>
+        </li>
+        <li onClick={() => props.handlePageChange(3)} className="page-item">
+          <a className="page-link" href="#">
+            3
+          </a>
+        </li>
+        <li onClick={() => props.handlePageChange(4)} className="page-item">
+          <a className="page-link" href="#">
+            4
+          </a>
+        </li>
+        <li onClick={() => props.handlePageChange(5)} className="page-item">
+          <a className="page-link" href="#">
+            5
+          </a>
+        </li>
+        <li onClick={() => props.handlePageChange(6)} className="page-item">
+          <a className="page-link" href="#">
+            6
+          </a>
+        </li>
+        <li onClick={() => props.handlePageChange(7)} className="page-item">
+          <a className="page-link" href="#">
+            7
+          </a>
+        </li>
+        <li onClick={() => props.handlePageChange(8)} className="page-item">
+          <a className="page-link" href="#">
+            8
+          </a>
+        </li>
+        <li onClick={() => props.handlePageChange(9)} className="page-item">
+          <a className="page-link" href="#">
+            9
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default StarWarsPagination;
