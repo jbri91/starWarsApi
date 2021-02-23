ac0e8d0c6390e6b05c3b5fe7fdd8c9f89eb049df
Johnny Briones <jbrrii91@gmail.com>
2021-02-22 22:22:03 -0500

    DRY

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import React from "react";
 import Table from "./components/Table";
 import "./App.css";
 import StarWarsPagination from "./components/StarWarsPagination";
-import UserInput from './components/UserInput'
-
+import UserInput from "./components/UserInput";
 
 class App extends React.Component {
   constructor(props) {
@@ -20,10 +19,9 @@ class App extends React.Component {
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  onChange = e =>{
-  this.setState({ search : e.target.value })
-  
-}
+  onChange = (e) => {
+    this.setState({ search: e.target.value });
+  };
 
   handlePageChange(pageNumber) {
     this.setState({
@@ -36,76 +34,58 @@ class App extends React.Component {
       this.handleDataRequest();
     }
   }
-  
+
   componentDidMount() {
     this.handleDataRequest();
   }
 
   async handleSubmit(e) {
     e.preventDefault();
-
-
-    const searchCharacters = await fetch(
-      `https://swapi.dev/api/people/?search=${this.state.search}`
-    ).then(response => response.json())
-    .then(character => character.results)
-    .catch((error) => {console.error('Error:', error)})
-
-
-for(let i=0; i < searchCharacters.length; i++){
-    if(searchCharacters.length > 0){
-     searchCharacters[i].homeworld =  await fetch(searchCharacters[i].homeworld.replace('http', 'https'))
-      .then(response => response.json())
-      .then(homeworld => homeworld.name)
-      .catch((error) => {console.error('Error:', error)})
-    } 
-      if(searchCharacters[i].species.length > 0){
-       searchCharacters[i].species = await fetch(searchCharacters[i].species.toString().replace('http', 'https'))
-        .then(response => response.json())
-        .then(species => species.name)
-        .catch((error) => {console.error('Error:', error)})
-      } else {
-        searchCharacters[i].species = 'Human'
-      }}
-      this.setState({
-        loading: false,
-        characters: searchCharacters,
-      })
-}
-  
+    this.getCharacters(  `https://swapi.dev/api/people/?search=${this.state.search}`)
+  }
 
   async handleDataRequest() {
+    this.getCharacters(
+      `https://swapi.dev/api/people/?page=${this.state.activePage}`
+    );
+  }
+
+  async getCharacters(url) {
     this.setState({
       loading: true,
     });
 
-    const characters = await fetch(
-      `https://swapi.dev/api/people/?page=${this.state.activePage}`
-    )
+    const characters = await fetch(url)
       .then((people) => people.json())
-     .then((character) => character.results)
+      .then((character) => character.results)
       .catch((error) => {
         console.error("Error:", error);
       });
 
-
     for (let i = 0; i < characters.length; i++) {
-     characters[i].homeworld = await fetch(characters[i].homeworld.replace('http', 'https'))
-        .then((response) => response.json())
-        .then((homeworld) => homeworld.name)
-        .catch((error) => {
-          console.error('Error:', error)
-      });;
-      if (characters[i].species.length > 0 ) {
-        characters[i].species = await fetch(characters[i].species.toString().replace('http', 'https'))
+      if (characters.length > 0) {
+        characters[i].homeworld = await fetch(
+          characters[i].homeworld.replace("http", "https")
+        )
+          .then((response) => response.json())
+          .then((homeworld) => homeworld.name)
+          .catch((error) => {
+            console.error("Error:", error);
+          });
+      }
+
+      if (characters[i].species.length > 0) {
+        characters[i].species = await fetch(
+          characters[i].species.toString().replace("http", "https")
+        )
           .then((response) => response.json())
           .then((species) => species.name)
           .catch((error) => {
-            console.error('Error:', error)
-        });
+            console.error("Error:", error);
+          });
       } else {
         characters[i].species = "Human";
-      } 
+      }
     }
     this.setState({
       loading: false,
@@ -125,14 +105,14 @@ for(let i=0; i < searchCharacters.length; i++){
           Star Wars API
         </h1>
         <form onSubmit={this.handleSubmit}>
-      <UserInput
-       onChange={this.onChange}
-       size="50"
-       placeholder="Who do you seek?"
-       name="userInput" 
-       />
-       </form>
-       <br />
+          <UserInput
+            onChange={this.onChange}
+            size="50"
+            placeholder="Who do you seek?"
+            name="userInput"
+          />
+        </form>
+        <br />
         {this.state.loading ? (
           <h1 style={{ color: "yellow" }}>Loading...</h1>
         ) : (
@@ -141,8 +121,6 @@ for(let i=0; i < searchCharacters.length; i++){
         <br />
         <StarWarsPagination
           activePage={this.state.activePage}
-          itemsCountPerPage={10}
-          totalItemsCount={82}
           pageRangeDisplayed={10}
           handlePageChange={this.handlePageChange}
         />
--- src/components/Page.js
+++ src/components/Page.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Page(props) {
   return (
     <li onClick={() => props.onPageClick(props.pageNum)} className="page-item">
-      <a className="page-link" href="#">
+      <a className="page-link">
         {props.pageNum}
       </a>
     </li>
